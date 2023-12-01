e2f8b66aa0568cfb16c04e42e48da9a07e7f4224
GRPS GANEGODA <piumisarangag@gmail.com>
2023-12-01 22:50:37 +0530

    read from database functions creat

--- app/Controller/Home.php
+++ app/Controller/Home.php
@@ -1,29 +1,19 @@
 <?php
-//require "app/Core/controller.php";
+
 class Home extends Controller
 {
-
-   /* public function index(){
-        $this->view('home');
-        //echo "home page";
-
-    }*/
-
-
     public function index()
-    {    
-        $db = new Database();
-        $db->query();
+    {
+       
+
+        
         $data['title'] = "Home";
-        $viewPath = $this->view('home',$data);
-        if ($viewPath) {
-            // Include the view file to display its content
-            require $viewPath;
-        } else {
-            // Handle the case where the view file doesn't exist
-            echo "Could not find the view file";
-        }
+       
+        $viewPath = $this->view('home', $data);
+        require ($viewPath);
+
+        
     }
-    
-    
 }
+
+?>
--- app/Core/database.php
+++ app/Core/database.php
@@ -4,14 +4,53 @@ class Database
 {
     private function connect()
     {
-        $str =DBDRIVER.":hostname=".DBHOST.";dbname=".DBNAME;
+        $str = DBDRIVER . ":host=" . DBHOST . ";dbname=" . DBNAME;
     
         return new PDO($str,DBUSER,DBPASS);
-
-    }
-    public function query(){
-      $con = $this->connect();  
-      show($con);
     }
 
+    public function query($query, $data = [], $type = 'object')
+    {
+        $con = $this->connect(); 
+
+        $stm = $con -> prepare($query);
+         
+        if($stm){
+            $check = $stm->execute($data);
+            if($check){
+                 $type = PDO::FETCH_OBJ;
+                 if($type == 'object'){
+                    $type = PDO::FETCH_ASSOC;
+                 }else{
+                    $type = PDO::FETCH_OBJ;
+
+                 }
+                 
+                 $result = $stm ->fetchAll($type);
+
+                 if(is_array($result)&& count($result)> 0){
+                    return $result;
+
+                 }
+            }
+        }
+
+        // Return false if any step fails
+        return false;
+    }
+    public function create_tables(){
+        $query = "
+            CREATE TABLE IF NOT EXISTS `users` (
+            `ID` int(11) NOT NULL AUTO_INCREMENT,
+            `email` varchar(100) NOT NULL,
+            `password` varchar(255) NOT NULL,
+            `date` date NOT NULL,
+            PRIMARY KEY (`ID`)
+           ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+        
+        ";
+        $this->query($query);
+    }
 }
+
+?>
